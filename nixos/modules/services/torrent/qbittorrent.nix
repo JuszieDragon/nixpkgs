307438334e9f8b3b22baa6ed865121cd18392c51
Justin Johnson <justin.h.j.johnson@gmail.com>
2026-02-10 13:05:02 +1100

    add option to configure qBittorrent categories

--- nixos/modules/services/torrent/qbittorrent.nix
+++ nixos/modules/services/torrent/qbittorrent.nix
@@ -7,7 +7,7 @@
 }:
 let
   cfg = config.services.qbittorrent;
-  inherit (builtins) concatStringsSep isAttrs isString;
+  inherit (builtins) concatStringsSep isAttrs isString toJSON;
   inherit (lib)
     literalExpression
     getExe
@@ -20,7 +20,9 @@ let
     replaceString
     collect
     mapAttrsRecursive
+    optionalAttrs
     optionals
+    recursiveUpdate
     ;
   inherit (lib.types)
     str
@@ -53,7 +55,10 @@ let
       else
         mkKeyValueDefault { } sep k v;
   };
-  configFile = pkgs.writeText "qBittorrent.conf" (gendeepINI cfg.serverConfig);
+  categoriesEnableString = "@Variant(\\0\\0\\0\\b\\0\\0\\0\\x3\\0\\0\\0\\b\\0M\\0i\\0s\\0\\x63\\0\\0\\0\\n\\0\\0\\0\\x1e\\0/\\0\\x64\\0o\\0w\\0n\\0l\\0o\\0\\x61\\0\\x64\\0s\\0/\\0M\\0i\\0s\\0\\x63\\0\\0\\0\\f\\0r\\0\\x61\\0\\x64\\0\\x61\\0r\\0r\\0\\0\\0\\n\\0\\0\\0\\x34\\0/\\0\\x64\\0o\\0w\\0n\\0l\\0o\\0\\x61\\0\\x64\\0s\\0/\\0U\\0n\\0s\\0o\\0r\\0t\\0\\x65\\0\\x64\\0/\\0r\\0\\x61\\0\\x64\\0\\x61\\0r\\0r\\0\\0\\0\\x12\\0t\\0v\\0-\\0s\\0o\\0n\\0\\x61\\0r\\0r\\0\\0\\0\\n\\0\\0\\0\\x34\\0/\\0\\x64\\0o\\0w\\0n\\0l\\0o\\0\\x61\\0\\x64\\0s\\0/\\0U\\0n\\0s\\0o\\0r\\0t\\0\\x65\\0\\x64\\0/\\0s\\0o\\0n\\0\\x61\\0r\\0r)";
+  catagoriesConfig = optionalAttrs (cfg.categories != { } && cfg.serverConfig != { }) { BitTorrent.Session.Categories = categoriesEnableString; };
+  configFile = pkgs.writeText "qBittorrent.conf" (gendeepINI (recursiveUpdate cfg.serverConfig catagoriesConfig));
+  categoriesFile = pkgs.writeText "categories.json" (toJSON cfg.categories);
 in
 {
   options.services.qbittorrent = {
@@ -131,6 +136,21 @@ in
       '';
     };
 
+    categories = mkOption {
+      type = submodule {
+        freeformType = attrsOf (attrsOf anything);
+      };
+      default = { };
+      description = ''
+        The categories for qBittorrent to use, mapped to the categories.json file in the profile.
+      '';
+      example = literalExpression ''
+        {
+          "TV Shows".save_path = "/var/lib/torrents/tv";
+        }
+      '';
+    };
+
     extraArgs = mkOption {
       type = listOf str;
       default = [ ];
@@ -158,6 +178,11 @@ in
             mode = "1400";
             inherit (cfg) user group;
             argument = "${configFile}";
+          };
+          "${cfg.profileDir}/qBittorrent/config/categories.json"."L+" = mkIf (cfg.categories != { }) {
+            mode = "1400";
+            inherit (cfg) user group;
+            argument = "${categoriesFile}";
           };
         };
       };
